59938184f3f972772d829492960df8efc581b2b0
Lukas <57566886+LukasL28@users.noreply.github.com>
2022-01-24 19:50:17 +0100

    init

---
+++ __resource.lua
@@ -0,0 +1,15 @@
+files {
+    'index.html',
+    'style.css',
+    'bankgothic.ttf',
+    'animation.js',
+    'Bild1.jpg',
+    'Bild2.jpg',
+    'Bild3.jpg',
+    'Bild4.jpg',
+    'muke.mp3'
+}
+
+loadscreen 'index.html'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
